f88e5505b41c86d2f1fa7aa9fbe1e5241833836e
messense <messense@icloud.com>
2019-06-19 11:18:59 +0800

    Add bench_cut_with_hmm

--- benches/jieba_benchmark.rs
+++ benches/jieba_benchmark.rs
@@ -7,17 +7,29 @@ use jieba_rs::Jieba;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref jieba: Jieba = Jieba::new();
+    static ref JIEBA: Jieba = Jieba::new();
 }
 
 fn bench_cut_no_hmm(sentence: &str) {
-    jieba.cut(sentence, false);
+    JIEBA.cut(sentence, false);
+}
+
+fn bench_cut_with_hmm(sentence: &str) {
+    JIEBA.cut(sentence, true);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("jieba cut", |b| {
+    c.bench_function("jieba cut no hmm", |b| {
         b.iter(|| {
             bench_cut_no_hmm(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ))
+        })
+    });
+
+    c.bench_function("jieba cut with hmm", |b| {
+        b.iter(|| {
+            bench_cut_with_hmm(black_box(
                 "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
             ))
         })
