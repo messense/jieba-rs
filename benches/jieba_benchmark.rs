41f83c0eadafae23111869da357edba98070d378
messense <messense@icloud.com>
2019-06-20 17:04:54 +0800

    More micro-benchmarks

--- benches/jieba_benchmark.rs
+++ benches/jieba_benchmark.rs
@@ -3,35 +3,99 @@ extern crate criterion;
 
 use criterion::black_box;
 use criterion::Criterion;
-use jieba_rs::Jieba;
 use lazy_static::lazy_static;
+use jieba_rs::{Jieba, TokenizeMode};
+#[cfg(any(feature = "tfidf", feature = "textrank"))]
+use jieba_rs::KeywordExtract;
+#[cfg(feature = "tfidf")]
+use jieba_rs::TFIDF;
+#[cfg(feature = "textrank")]
+use jieba_rs::TextRank;
 
 lazy_static! {
     static ref JIEBA: Jieba = Jieba::new();
 }
-
-fn bench_cut_no_hmm(sentence: &str) {
-    JIEBA.cut(sentence, false);
+#[cfg(feature = "tfidf")]
+lazy_static! {
+    static ref TFIDF_EXTRACTOR: TFIDF<'static> = TFIDF::new_with_jieba(&JIEBA);
 }
-
-fn bench_cut_with_hmm(sentence: &str) {
-    JIEBA.cut(sentence, true);
+#[cfg(feature = "textrank")]
+lazy_static! {
+    static ref TEXTRANK_EXTRACTOR: TextRank<'static> = TextRank::new_with_jieba(&JIEBA);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("jieba cut no hmm", |b| {
         b.iter(|| {
-            bench_cut_no_hmm(black_box(
+            JIEBA.cut(black_box(
                 "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-            ))
+            ), false)
         })
     });
 
     c.bench_function("jieba cut with hmm", |b| {
         b.iter(|| {
-            bench_cut_with_hmm(black_box(
+            JIEBA.cut(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), true)
+        })
+    });
+
+    c.bench_function("jieba cut_all", |b| {
+        b.iter(|| {
+            JIEBA.cut_all(black_box(
                 "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
             ))
+        })
+    });
+
+    c.bench_function("jieba cut_for_search", |b| {
+        b.iter(|| {
+            JIEBA.cut_for_search(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), true)
+        })
+    });
+
+    c.bench_function("jieba tokenize default mode", |b| {
+        b.iter(|| {
+            JIEBA.tokenize(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), TokenizeMode::Default, true)
+        })
+    });
+
+    c.bench_function("jieba tokenize search mode", |b| {
+        b.iter(|| {
+            JIEBA.tokenize(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), TokenizeMode::Search, true)
+        })
+    });
+
+    c.bench_function("jieba tag", |b| {
+        b.iter(|| {
+            JIEBA.tag(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), true)
+        })
+    });
+
+    #[cfg(feature = "tfidf")]
+    c.bench_function("jieba tfidf", |b| {
+        b.iter(|| {
+            TFIDF_EXTRACTOR.extract_tags(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), 3, Vec::new())
+        })
+    });
+
+    #[cfg(feature = "textrank")]
+    c.bench_function("jieba textrank", |b| {
+        b.iter(|| {
+            TEXTRANK_EXTRACTOR.extract_tags(black_box(
+                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
+            ), 3, Vec::new())
         })
     });
 }
