cea6046e21d25d8f55d7e066f88c3c7432d16973
messense <messense@icloud.com>
2019-07-02 10:09:21 +0800

    Measure throughput in benchmark

--- benches/jieba_benchmark.rs
+++ benches/jieba_benchmark.rs
@@ -1,8 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::black_box;
-use criterion::Criterion;
+use criterion::{black_box, Benchmark, Criterion, Throughput};
 use jieba_rs::{Jieba, KeywordExtract, TextRank, TokenizeMode, TFIDF};
 use lazy_static::lazy_static;
 use rand::Rng;
@@ -17,6 +16,8 @@ lazy_static! {
     static ref TFIDF_EXTRACTOR: TFIDF<'static> = TFIDF::new_with_jieba(&JIEBA);
     static ref TEXTRANK_EXTRACTOR: TextRank<'static> = TextRank::new_with_jieba(&JIEBA);
 }
+static SENTENCE: &str =
+    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。";
 
 fn bench_dag_with_btree(sentence: &str) {
     let mut dag: BTreeMap<usize, SmallVec<[usize; 5]>> = BTreeMap::new();
@@ -79,121 +80,81 @@ fn bench_dag_with_vec(sentence: &str) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("jieba cut no hmm", |b| {
-        b.iter(|| {
-            JIEBA.cut(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                false,
-            )
+    c.bench(
+        "jieba cut",
+        Benchmark::new("no hmm", |b| b.iter(|| JIEBA.cut(black_box(SENTENCE), false)))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
+
+    c.bench(
+        "jieba cut",
+        Benchmark::new("with hmm", |b| b.iter(|| JIEBA.cut(black_box(SENTENCE), true)))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
+
+    c.bench(
+        "jieba",
+        Benchmark::new("cut_all", |b| b.iter(|| JIEBA.cut_all(black_box(SENTENCE))))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
+
+    c.bench(
+        "dag",
+        Benchmark::new("with btree", |b| b.iter(|| bench_dag_with_btree(black_box(SENTENCE))))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
+
+    c.bench(
+        "dag",
+        Benchmark::new("with vec", |b| b.iter(|| bench_dag_with_vec(black_box(SENTENCE))))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
+
+    c.bench(
+        "jieba",
+        Benchmark::new("cut_for_search", |b| {
+            b.iter(|| JIEBA.cut_for_search(black_box(SENTENCE), true))
         })
-    });
+        .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 
-    c.bench_function("jieba cut with hmm", |b| {
-        b.iter(|| {
-            JIEBA.cut(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                true,
-            )
+    c.bench(
+        "jieba tokenize",
+        Benchmark::new("default mode", |b| {
+            b.iter(|| JIEBA.tokenize(black_box(SENTENCE), TokenizeMode::Default, true))
         })
-    });
+        .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 
-    c.bench_function("jieba cut_all", |b| {
-        b.iter(|| {
-            JIEBA.cut_all(black_box(
-                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-            ))
-        });
-    });
-
-    c.bench_function("dag with btree", |b| {
-        b.iter(|| {
-            bench_dag_with_btree(black_box(
-                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-            ))
-        });
-    });
-
-    c.bench_function("dag with vec", |b| {
-        b.iter(|| {
-            bench_dag_with_vec(black_box(
-                "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-            ))
-        });
-    });
-
-    c.bench_function("jieba cut_for_search", |b| {
-        b.iter(|| {
-            JIEBA.cut_for_search(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                true,
-            )
+    c.bench(
+        "jieba tokenize",
+        Benchmark::new("search mode", |b| {
+            b.iter(|| JIEBA.tokenize(black_box(SENTENCE), TokenizeMode::Search, true))
         })
-    });
+        .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 
-    c.bench_function("jieba tokenize default mode", |b| {
-        b.iter(|| {
-            JIEBA.tokenize(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                TokenizeMode::Default,
-                true,
-            )
-        })
-    });
+    c.bench(
+        "jieba",
+        Benchmark::new("tag", |b| b.iter(|| JIEBA.tag(black_box(SENTENCE), true)))
+            .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 
-    c.bench_function("jieba tokenize search mode", |b| {
-        b.iter(|| {
-            JIEBA.tokenize(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                TokenizeMode::Search,
-                true,
-            )
+    c.bench(
+        "jieba",
+        Benchmark::new("tfidf", |b| {
+            b.iter(|| TFIDF_EXTRACTOR.extract_tags(black_box(SENTENCE), 3, Vec::new()))
         })
-    });
+        .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 
-    c.bench_function("jieba tag", |b| {
-        b.iter(|| {
-            JIEBA.tag(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                true,
-            )
+    c.bench(
+        "jieba",
+        Benchmark::new("textrank", |b| {
+            b.iter(|| TEXTRANK_EXTRACTOR.extract_tags(black_box(SENTENCE), 3, Vec::new()))
         })
-    });
-
-    c.bench_function("jieba tfidf", |b| {
-        b.iter(|| {
-            TFIDF_EXTRACTOR.extract_tags(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                3,
-                Vec::new(),
-            )
-        })
-    });
-
-    c.bench_function("jieba textrank", |b| {
-        b.iter(|| {
-            TEXTRANK_EXTRACTOR.extract_tags(
-                black_box(
-                    "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。",
-                ),
-                3,
-                Vec::new(),
-            )
-        })
-    });
+        .throughput(Throughput::Bytes(SENTENCE.len() as u32)),
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
