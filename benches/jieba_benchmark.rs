448488a85b4bb687391f76a552b52e668db5c2af
Paul Meng <me@paulme.ng>
2019-07-11 15:40:05 +0800

    Implement StaticSparseDAG

--- benches/jieba_benchmark.rs
+++ benches/jieba_benchmark.rs
@@ -4,9 +4,6 @@ extern crate criterion;
 use criterion::{black_box, Benchmark, Criterion, ParameterizedBenchmark, Throughput};
 use jieba_rs::{Jieba, KeywordExtract, TextRank, TokenizeMode, TFIDF};
 use lazy_static::lazy_static;
-use rand::Rng;
-use smallvec::SmallVec;
-use std::collections::btree_map::BTreeMap;
 
 #[cfg(unix)]
 #[global_allocator]
@@ -19,66 +16,6 @@ lazy_static! {
 }
 static SENTENCE: &str =
     "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。";
-
-fn bench_dag_with_btree(sentence: &str) {
-    let mut dag: BTreeMap<usize, SmallVec<[usize; 5]>> = BTreeMap::new();
-    let word_count = sentence.len();
-    let mut rng = rand::thread_rng();
-
-    for i in 0..(word_count - 1) {
-        let mut tmplist = SmallVec::new();
-
-        let number_of_node = rng.gen_range(0, 6);
-        for _ in 0..number_of_node {
-            let x = rng.gen_range(i + 1, word_count + 1);
-            tmplist.push(x);
-        }
-
-        dag.insert(i, tmplist);
-    }
-
-    dag.insert(word_count - 1, SmallVec::new());
-
-    let mut route = Vec::with_capacity(word_count + 1);
-    for _ in 0..=word_count {
-        route.push(0);
-    }
-
-    for i in (0..word_count).rev() {
-        let x = dag[&i].iter().map(|x| x + 1).max().unwrap_or(0);
-        route.push(x);
-    }
-}
-
-fn bench_dag_with_vec(sentence: &str) {
-    let word_count = sentence.len();
-    let mut dag: Vec<SmallVec<[usize; 5]>> = Vec::with_capacity(word_count);
-    let mut rng = rand::thread_rng();
-
-    for i in 0..(word_count - 1) {
-        let mut tmplist = SmallVec::new();
-
-        let number_of_node = rng.gen_range(0, 6);
-        for _ in 0..number_of_node {
-            let x = rng.gen_range(i + 1, word_count + 1);
-            tmplist.push(x);
-        }
-
-        dag.push(tmplist);
-    }
-
-    dag.insert(word_count - 1, SmallVec::new());
-
-    let mut route = Vec::with_capacity(word_count + 1);
-    for _ in 0..=word_count {
-        route.push(0);
-    }
-
-    for i in (0..word_count).rev() {
-        let x = dag[i].iter().map(|x| x + 1).max().unwrap_or(0);
-        route.push(x);
-    }
-}
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench(
@@ -94,13 +31,6 @@ fn criterion_benchmark(c: &mut Criterion) {
             b.iter(|| JIEBA.cut_for_search(black_box(i), true))
         })
         .throughput(|i| Throughput::Bytes(i.len() as u32)),
-    );
-
-    c.bench(
-        "dag",
-        ParameterizedBenchmark::new("with btree", |b, i| b.iter(|| bench_dag_with_btree(i)), vec![SENTENCE])
-            .with_function("with vec", |b, i| b.iter(|| bench_dag_with_vec(i)))
-            .throughput(|i| Throughput::Bytes(i.len() as u32)),
     );
 
     c.bench(
---
+++ src/sparse_dag.rs
@@ -0,0 +1,101 @@
+use hashbrown::HashMap;
+
+pub(crate) struct StaticSparseDAG {
+    array: Vec<usize>,
+    start_pos: HashMap<usize, usize>,
+    size_hint_for_iterator: usize,
+    curr_insertion_len: usize,
+}
+
+pub struct EdgeIter<'a> {
+    dag: &'a StaticSparseDAG,
+    cursor: usize,
+}
+
+impl<'a> Iterator for EdgeIter<'a> {
+    type Item = usize;
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.dag.size_hint_for_iterator))
+    }
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.dag.array[self.cursor] == 0 {
+            self.cursor += 1;
+            None
+        } else {
+            let v = self.dag.array[self.cursor] - 1;
+            self.cursor += 1;
+            Some(v)
+        }
+    }
+}
+
+impl StaticSparseDAG {
+    pub(crate) fn with_size_hint(hint: usize) -> Self {
+        StaticSparseDAG {
+            array: Vec::with_capacity(hint * 5),
+            start_pos: HashMap::new(),
+            size_hint_for_iterator: 0,
+            curr_insertion_len: 0,
+        }
+    }
+
+    #[inline]
+    pub(crate) fn start(&mut self, from: usize) {
+        let idx = self.array.len();
+        self.curr_insertion_len = 0;
+        self.start_pos.insert(from, idx);
+    }
+
+    #[inline]
+    pub(crate) fn insert(&mut self, to: usize) {
+        self.curr_insertion_len += 1;
+        self.array.push(to + 1);
+    }
+
+    #[inline]
+    pub(crate) fn commit(&mut self) {
+        self.size_hint_for_iterator = std::cmp::max(self.curr_insertion_len, self.size_hint_for_iterator);
+        self.array.push(0);
+    }
+
+    #[inline]
+    pub(crate) fn iter_edges(&self, from: usize) -> EdgeIter {
+        let cursor = self.start_pos.get(&from).unwrap().to_owned();
+
+        EdgeIter { dag: self, cursor }
+    }
+
+    pub(crate) fn clear(&mut self) {
+        self.array.clear();
+        self.start_pos.clear();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_static_sparse_dag() {
+        let mut dag = StaticSparseDAG::with_size_hint(5);
+        let mut ans: Vec<Vec<usize>> = vec![Vec::new(); 5];
+        for i in 0..=3 {
+            dag.start(i);
+            for j in (i + 1)..=4 {
+                ans[i].push(j);
+                dag.insert(j);
+            }
+
+            dag.commit()
+        }
+
+        assert_eq!(dag.size_hint_for_iterator, 4);
+
+        for i in 0..=3 {
+            let edges: Vec<usize> = dag.iter_edges(i).collect();
+            assert_eq!(ans[i], edges);
+        }
+    }
+}
