2328010209c76fcc6dc10f62a9f9caf2d8ef4f10
messense <messense@icloud.com>
2021-01-29 18:15:17 +0800

    Update criterion and use benchmark group API

--- benches/jieba_benchmark.rs
+++ benches/jieba_benchmark.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::{black_box, Benchmark, Criterion, ParameterizedBenchmark, Throughput};
+use criterion::{black_box, Criterion, Throughput};
 use jieba_rs::{Jieba, KeywordExtract, TextRank, TokenizeMode, TFIDF};
 use lazy_static::lazy_static;
 
@@ -17,52 +17,40 @@ lazy_static! {
 static SENTENCE: &str = "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。";
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench(
-        "jieba cut",
-        ParameterizedBenchmark::new(
-            "no hmm",
-            |b, i| b.iter(|| JIEBA.cut(black_box(i), false)),
-            vec![SENTENCE],
-        )
-        .with_function("with hmm", |b, i| b.iter(|| JIEBA.cut(black_box(i), true)))
-        .with_function("cut_all", |b, i| b.iter(|| JIEBA.cut_all(black_box(i))))
-        .with_function("cut_for_search", |b, i| {
-            b.iter(|| JIEBA.cut_for_search(black_box(i), true))
-        })
-        .throughput(|i| Throughput::Bytes(i.len() as u64)),
-    );
+    let mut group = c.benchmark_group("jieba cut");
+    group.throughput(Throughput::Bytes(SENTENCE.len() as u64));
+    group.bench_function("no hmm", |b| b.iter(|| JIEBA.cut(black_box(SENTENCE), false)));
+    group.bench_function("with hmm", |b| b.iter(|| JIEBA.cut(black_box(SENTENCE), true)));
+    group.bench_function("cut_all", |b| b.iter(|| JIEBA.cut_all(black_box(SENTENCE))));
+    group.bench_function("cut_for_search", |b| {
+        b.iter(|| JIEBA.cut_for_search(black_box(SENTENCE), true))
+    });
+    group.finish();
 
-    c.bench(
-        "jieba tokenize",
-        ParameterizedBenchmark::new(
-            "default mode",
-            |b, i| b.iter(|| JIEBA.tokenize(black_box(i), TokenizeMode::Default, true)),
-            vec![SENTENCE],
-        )
-        .with_function("search mode", |b, i| {
-            b.iter(|| JIEBA.tokenize(black_box(i), TokenizeMode::Search, true))
-        })
-        .throughput(|i| Throughput::Bytes(i.len() as u64)),
-    );
+    let mut group = c.benchmark_group("jieba tokenize");
+    group.throughput(Throughput::Bytes(SENTENCE.len() as u64));
+    group.bench_function("default mode", |b| {
+        b.iter(|| JIEBA.tokenize(black_box(SENTENCE), TokenizeMode::Default, true))
+    });
+    group.bench_function("search mode", |b| {
+        b.iter(|| JIEBA.tokenize(black_box(SENTENCE), TokenizeMode::Search, true))
+    });
+    group.finish();
 
-    c.bench(
-        "jieba",
-        Benchmark::new("tag", |b| b.iter(|| JIEBA.tag(black_box(SENTENCE), true)))
-            .throughput(Throughput::Bytes(SENTENCE.len() as u64)),
-    );
+    let mut group = c.benchmark_group("jieba");
+    group.throughput(Throughput::Bytes(SENTENCE.len() as u64));
+    group.bench_function("tag", |b| b.iter(|| JIEBA.tag(black_box(SENTENCE), true)));
+    group.finish();
 
-    c.bench(
-        "jieba extract keywords",
-        ParameterizedBenchmark::new(
-            "tfidf",
-            |b, i| b.iter(|| TFIDF_EXTRACTOR.extract_tags(black_box(i), 3, Vec::new())),
-            vec![SENTENCE],
-        )
-        .with_function("textrank", |b, i| {
-            b.iter(|| TEXTRANK_EXTRACTOR.extract_tags(black_box(i), 3, Vec::new()))
-        })
-        .throughput(|i| Throughput::Bytes(i.len() as u64)),
-    );
+    let mut group = c.benchmark_group("jieba extract keywords");
+    group.throughput(Throughput::Bytes(SENTENCE.len() as u64));
+    group.bench_function("tfidf", |b| {
+        b.iter(|| TFIDF_EXTRACTOR.extract_tags(black_box(SENTENCE), 3, Vec::new()))
+    });
+    group.bench_function("textrank", |b| {
+        b.iter(|| TEXTRANK_EXTRACTOR.extract_tags(black_box(SENTENCE), 3, Vec::new()))
+    });
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
