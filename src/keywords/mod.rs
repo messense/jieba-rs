1391b3d9539485df9f571ab6171f539a182838be
Albert J. Wong <awong.dev@gmail.com>
2024-04-12 21:12:31 -0700

    Add KeywordExtractConfigBuilder (#104)

--- src/keywords/mod.rs
+++ src/keywords/mod.rs
@@ -32,76 +32,110 @@ pub struct Keyword {
     pub weight: f64,
 }
 
-#[derive(Debug, Clone)]
+/// Creates a KeywordExtractConfig state that contains filter criteria as
+/// well as segmentation configuration for use by keyword extraction
+/// implementations.
+///
+/// Use KeywordExtractConfigBuilder to change the defaults.
+///
+/// # Examples
+/// ```
+///    use jieba_rs::KeywordExtractConfig;
+///
+///    let mut config = KeywordExtractConfig::default();
+///    assert!(config.stop_words().contains("the"));
+///    assert!(!config.stop_words().contains("FakeWord"));
+///    assert!(!config.use_hmm());
+///    assert_eq!(2, config.min_keyword_length());
+///
+///    let built_default = KeywordExtractConfig::builder().build().unwrap();
+///    assert_eq!(config, built_default);
+///
+///    let changed = KeywordExtractConfig::builder()
+///        .add_stop_word("FakeWord".to_string())
+///        .remove_stop_word("the")
+///        .use_hmm(true)
+///        .min_keyword_length(10)
+///        .build().unwrap();
+///
+///    assert!(!changed.stop_words().contains("the"));
+///    assert!(changed.stop_words().contains("FakeWord"));
+///    assert!(changed.use_hmm());
+///    assert_eq!(10, changed.min_keyword_length());
+/// ```
+#[derive(Builder, Debug, Clone, PartialEq)]
 pub struct KeywordExtractConfig {
+    #[builder(default = "self.default_stop_words()?", setter(custom))]
     stop_words: BTreeSet<String>,
+
+    #[builder(default = "2")]
+    #[doc = r"Any segments less than this length will not be considered a Keyword"]
     min_keyword_length: usize,
+
+    #[builder(default = "false")]
+    #[doc = r"If true, fall back to hmm model if segment cannot be found in the dictionary"]
     use_hmm: bool,
 }
 
 impl KeywordExtractConfig {
-    /// Creates a KeywordExtractConfig state that contains filter criteria as
-    /// well as segmentation configuration for use by keyword extraction
-    /// implementations.
-    pub fn new(stop_words: BTreeSet<String>, min_keyword_length: usize, use_hmm: bool) -> Self {
-        KeywordExtractConfig {
-            stop_words,
-            min_keyword_length,
-            use_hmm,
-        }
-    }
-
-    /// Add a new stop word.
-    pub fn add_stop_word(&mut self, word: String) -> bool {
-        self.stop_words.insert(word)
-    }
-
-    /// Remove an existing stop word.
-    pub fn remove_stop_word(&mut self, word: &str) -> bool {
-        self.stop_words.remove(word)
-    }
-
-    /// Replace all stop words with new stop words set.
-    pub fn set_stop_words(&mut self, stop_words: BTreeSet<String>) {
-        self.stop_words = stop_words
+    pub fn builder() -> KeywordExtractConfigBuilder {
+        KeywordExtractConfigBuilder::default()
     }
 
     /// Get current set of stop words.
-    pub fn get_stop_words(&self) -> &BTreeSet<String> {
+    pub fn stop_words(&self) -> &BTreeSet<String> {
         &self.stop_words
     }
 
     /// True if hmm is used during segmentation in `extract_tags`.
-    pub fn get_use_hmm(&self) -> bool {
+    pub fn use_hmm(&self) -> bool {
         self.use_hmm
     }
 
-    /// Sets whether or not to use hmm during segmentation in `extract_tags`.
-    pub fn set_use_hmm(&mut self, use_hmm: bool) {
-        self.use_hmm = use_hmm
-    }
-
     /// Gets the minimum number of Unicode Scalar Values required per keyword.
-    pub fn get_min_keyword_length(&self) -> usize {
+    pub fn min_keyword_length(&self) -> usize {
         self.min_keyword_length
-    }
-
-    /// Sets the minimum number of Unicode Scalar Values required per keyword.
-    ///
-    /// The default is 2. There is likely not much reason to change this.
-    pub fn set_min_keyword_length(&mut self, min_keyword_length: usize) {
-        self.min_keyword_length = min_keyword_length
     }
 
     #[inline]
     pub(crate) fn filter(&self, s: &str) -> bool {
-        s.chars().count() >= self.min_keyword_length && !self.stop_words.contains(&s.to_lowercase())
+        s.chars().count() >= self.min_keyword_length() && !self.stop_words.contains(&s.to_lowercase())
+    }
+}
+
+impl KeywordExtractConfigBuilder {
+    fn default_stop_words(&self) -> Result<BTreeSet<String>, KeywordExtractConfigBuilderError> {
+        Ok(DEFAULT_STOP_WORDS.clone())
+    }
+
+    /// Add a new stop word.
+    pub fn add_stop_word(&mut self, word: String) -> &mut Self {
+        if self.stop_words.is_none() {
+            self.stop_words = Some(self.default_stop_words().unwrap());
+        }
+        self.stop_words.as_mut().unwrap().insert(word);
+        self
+    }
+
+    /// Remove an existing stop word.
+    pub fn remove_stop_word(&mut self, word: &str) -> &mut Self {
+        if self.stop_words.is_none() {
+            self.stop_words = Some(self.default_stop_words().unwrap());
+        }
+        self.stop_words.as_mut().unwrap().remove(word);
+        self
+    }
+
+    /// Replace all stop words with new stop words set.
+    pub fn set_stop_words(&mut self, stop_words: BTreeSet<String>) -> &mut Self {
+        self.stop_words = Some(stop_words);
+        self
     }
 }
 
 impl Default for KeywordExtractConfig {
-    fn default() -> Self {
-        KeywordExtractConfig::new(DEFAULT_STOP_WORDS.clone(), 2, false)
+    fn default() -> KeywordExtractConfig {
+        KeywordExtractConfigBuilder::default().build().unwrap()
     }
 }
 
